09b5f618d180b375984259773cf03c4dcda4f5fc
Edwin <edgoca2498@gmail.com>
2022-11-29 08:12:18 -0600

    :art: Add CRUD to products

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -10,5 +19,26 @@ export class ProductsController {
   @Get('/')
   getProducts(@Query('limit') limit = '100', @Query('offset') offset = '50') {
     return `Products limit:${limit} and offset: ${offset}`;
+  }
+
+  @Post()
+  create(@Body() payload) {
+    return {
+      message: 'Action to create',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return id;
   }
 }
