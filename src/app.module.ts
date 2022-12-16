e81716a461e1aaf0027f49243d7772d7a5409b1c
Edwin <edgoca2498@gmail.com>
2022-12-16 08:34:08 -0600

    :art: Add new use providers to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,34 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
+const API_KEY = '12345678';
+const API_KEY_PROD = 'zxcvbnm';
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        try {
+          const { data } = await lastValueFrom(
+            http.get('https://jsonplaceholder.typicode.com/todos'),
+          );
+          return data;
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
